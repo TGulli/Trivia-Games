989dade99f8b85ffd3f8c7cff7b9fa9c782d3509
hiba <hiba.benhaida@no.experis.no>
2021-02-19 16:22:08 +0100

    layout on result and calculate function is fixed with dummy data

--- trivia-games/src/components/Results.vue
+++ trivia-games/src/components/Results.vue
@@ -1,15 +1,65 @@
 <template>
 <div class="results">
-    <h4>Results</h4>
+    
+    <h2>Results</h2>
+    <div>       
+        <table class="table">
+        <tr>
+            <th>Question</th>
+            <th>Your Answer</th>
+            <th>The Rigth Answer</th>
+        </tr>
+        <tr v-for="value in testObj" :key="value.question">
+            <td> {{ value.question }}</td>
+            <td> {{ value.answered }}</td>
+            <td> {{ value.correct_answer }}</td>
+            
+        </tr>
+        </table>
+    </div>
+    
+    <h4 >Score : {{calculateScore()}}  </h4>
+
+    <router-link to="/questions" tag="button">Play Again</router-link>
+    
 </div>
 </template>
 
 <script>
 export default {
+     name: 'Questions',
+  data() {
+    return { testObj : [ {question: "Monday", correct_answer: "true", answered: "false"},
+{question: "Tuesday", correct_answer: "true", answered: "false"},
+{question: "Wednesday", correct_answer: "false", answered: "false"},
+{question: "Thursday?", correct_answer: "false", answered: "false"},
+{question: "Friday?", correct_answer: "true", answered: "false"},
+{question: "Saturday?", correct_answer: "false", answered: "false"},
+{question: "Sunday?", correct_answer: "true", answered: "false"}]
+    }
+  },
+   mounted() {
+  },
+  methods:{
+      calculateScore(){
+          let finalScore=100
+          let scoreUser=0
+          this.testObj.forEach(element => {
+              if(element.correct_answer === element.answered){
+                  scoreUser= +scoreUser + +10
+              }
+          });
+
+          return scoreUser+"/"+ finalScore
+      }
+  }
+    
 
 }
 </script>
 
 <style>
-
+.table{
+    text-align: center;
+}
 </style>
